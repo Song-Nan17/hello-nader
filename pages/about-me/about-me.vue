4c835b52bb6c2484bda8f5535d57d52a0ad81713
Song <songnan17@qq.com>
2022-03-08 09:52:14 +0800

    Add self assessments.

--- pages/about-me/about-me.vue
+++ pages/about-me/about-me.vue
@@ -1,9 +1,11 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="container">
+		<u-section title="自我评价" type="line">
+			<uni-list :border="false">
+				<uni-list-item :border="false" v-for="(selfAssessment,index) in selfAssessments" :show-extra-icon="true"
+					:extra-icon="extraIcon" :title="selfAssessment" :key="index" />
+			</uni-list>
+		</u-section>
 	</view>
 </template>
 
@@ -11,7 +13,17 @@
 	export default {
 		data() {
 			return {
-				title: '宋楠'
+				title: '宋楠',
+				extraIcon: {
+					color: '#707070',
+					size: '22',
+					type: 'forward',
+				},
+				selfAssessments: ['学习能力强，能根据工作需要快速学习各种技术语言，了解项目业务上下文，快速投入到新项目的生产中',
+					'责任心强、工作细致、认真、严谨，善于沟通与协调团队，做事踏实、有耐心，能够理解并服从安排',
+					'对技术有浓厚兴趣，喜欢并不断学习新技术',
+					'代码习惯好，注重clean code和code refactor'
+				]
 			}
 		},
 		onLoad() {
@@ -20,33 +32,19 @@
 		methods: {
 
 		}
-	}
+	};
 </script>
 
 <style>
-	.content {
+	.container {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
 	.text-area {
 		display: flex;
 		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
 	}
 </style>
